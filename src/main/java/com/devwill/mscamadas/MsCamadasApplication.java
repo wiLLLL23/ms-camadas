d1595aebdec1200d15f310590bda3703eda220ca
William Barbosa <will.barbosa23@gmail.com>
2024-11-19 22:29:18 -0300

    choreÃ: configuring ms-camadas project

---
+++ src/main/java/com/devwill/mscamadas/MsCamadasApplication.java
@@ -0,0 +1,12 @@
+package com.devwill.mscamadas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCamadasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsCamadasApplication.class, args);
+    }
+}
---
+++ src/test/java/com/devwill/mscamadas/MsCamadasApplicationTest.java
@@ -0,0 +1,14 @@
+package com.devwill.mscamadas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MsCamadasApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
